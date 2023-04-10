97493634ab0a0476302c365fd19092ea2f761089
srini047 <srinikethcr7@gmail.com>
2023-04-11 00:04:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Configure,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import './App.css';
+
+const searchClient = algoliasearch(
+  'YSNJEQILKB',
+  'ef768a413dab8f5b46866d6414c6554e'
+);
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="header-title">
+          <a href="/">naas-search-demo-app</a>
+        </h1>
+        <p className="header-subtitle">
+          using{' '}
+          <a href="https://github.com/algolia/react-instantsearch">
+            React InstantSearch
+          </a>
+        </p>
+      </header>
+
+      <div className="container">
+        <InstantSearch
+          searchClient={searchClient}
+          // apiKey="ef768a413dab8f5b46866d6414c6554e"
+          // appId="YSNJEQILKB"
+          indexName="naas-ai"
+        >
+          <Configure hitsPerPage={8} />
+          <div className="search-panel">
+            <div className="search-panel__filters"></div>
+
+            <div className="search-panel__results">
+              <SearchBox
+                className="searchbox"
+                translations={{
+                  placeholder: '',
+                }}
+              />
+              <Hits hitComponent={Hit} />
+
+              <div className="pagination">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
+    </div>
+  );
+}
+
+function Hit(props) {
+  return (
+    <article>
+      <h1>
+        <Highlight attribute="tool" hit={props.hit} />
+      </h1>
+    </article>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default App;
